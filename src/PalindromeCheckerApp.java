e8517ebcd9936c61fa5cd38e769bbfd8a2624075
srijeevikaa28 <srijeevikaa@gmail.com>
2026-02-27 15:34:07 +0530

    this is a develop branch

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,10 +2,6 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // Application Entry Point
-        System.out.println("WELCOME TO PALINDROME CHECKER MANAGEMENT SYSTEM");
-        System.out.println("VERSION 1.0");
-        System.out.println("SYSTEM INITIALIZED SUCCESSFULLY");
 
     }
 }
